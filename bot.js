e9cced87f2ff4baa0fa033900a9ec1daf53a8606
DESKTOP\Frank <fmiskuly19@jcu.edu>
2018-10-30 23:07:39 -0400

    new commands, trying to understand the api and new javascript stuff included in ES6

--- bot.js
+++ bot.js
@@ -2,19 +2,33 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const config = require("./config.json");
+
+//faux instance variables
+
+//what is the solution for this? How can I access the current guild without creating global variables
 const paddyID = "177562182463127563";
 
 bot.on("ready", () => {
-  console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`); 
+  console.log(`bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`); 
   bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
 });
 
-bot.on("raw", console.log);
+// bot.on("raw", (o) => {
+//   console.log(o);
+// });
+
+// bot.on("PRESENCE_UPDATE", (packet) => {
+//   console.log(`User with ID ${packet.d.user.id} has changed their presence.`);
+//   message.channel.send(`User with ID ${packet.d.user.id} has changed their presence.`);
+// });
+
+// bot.on("VOICE_STATE_UPDATE", async (packet) => {
+//   console.log(`User with ID ${packet.d.member.nick} has caused a voice_state_update.`);
+//   await message.channel.send(`User with ID ${packet.d.member.nick} has caused a voice_state_update.`);
+// });
 
 bot.on("guildCreate", guild => {
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  currGuild = guild.id;
-  console.log("current guild id: ", guild.id);
   bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
 });
 
@@ -27,8 +41,6 @@ bot.on("guildDelete", guild => {
 bot.on("message", async message => {
     
   if(message.author.bot) return;
-
-  //if(message.author.id !== 177280495317549056) return;
   
   if(message.content.indexOf(config.prefix) !== 0) return;
   
@@ -40,16 +52,10 @@ bot.on("message", async message => {
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
   }
 
-  if(command === "console.log") {
-
-  }
-
-  if(command === "console") {
+  if(command === "egan") {
     var currGuild = bot.guilds.find(guild => guild.id === paddyID);
-    var egan = currGuild.members.find(member => member.displayName === "poggers").user;
-    console.log("Egan: ",egan);
-    const m = await message.channel.send("Loading..");
-    m.edit(`Command executed`);
+    var egan = currGuild.members.find(member => member.displayName === "poggers").presence.status;
+    await message.channel.send(`Egan is ${egan}`);
   }
   
   if(command === "say") {
@@ -57,9 +63,13 @@ bot.on("message", async message => {
     message.delete().catch(O_o=>{}); 
     message.channel.send(sayMessage);
   }
+
+  if(command === "help") {
+    await message.channel.send("Ya aint gonna get any help here!");
+  }
   
   if(command === "kick") {
-    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["Server Owner"].includes(r.name)) )
       return message.reply("Sorry, you don't have permissions to use this!");
     
     let member = message.mentions.members.first() || message.guild.members.get(args[0]);
@@ -81,7 +91,7 @@ bot.on("message", async message => {
   
   if(command === "ban") {
 
-    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)))
       return message.reply("Sorry, you don't have permissions to use this!");
     
     let member = message.mentions.members.first();
@@ -100,6 +110,9 @@ bot.on("message", async message => {
   
   if(command === "purge") {
 
+    if(!message.member.roles.some(r=>["Server Owner"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+
     const deleteCount = parseInt(args[0], 10);
     
     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
@@ -108,6 +121,8 @@ bot.on("message", async message => {
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+
+      message.reply(`${message.author.tag} deleted ${deleteCount} messages from ${message.channel}`);
   }
 });
 
