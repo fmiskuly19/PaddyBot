e9c7cdb97e59b025da4518544c91fe49a65c9eac
DESKTOP\Frank <fmiskuly19@jcu.edu>
2018-11-05 01:30:01 -0500

    fixed status command to work with bots and users in applications such as spotify

--- commands/status.js
+++ commands/status.js
@@ -14,6 +14,8 @@ module.exports.run = async (bot, message,args) => {
             (p.status === "offline") ? 16711680 :
                 16098851;
 
+    console.log(p);
+
     //by default, only field is online status
     var fields = [{ "name": "Status", "value": p.status }]; 
 
@@ -22,20 +24,31 @@ module.exports.run = async (bot, message,args) => {
         let inGame = (p.game) ? p.game.name : "No";
         //add in game field
         fields.push({ "name": "In Game", "value": inGame })
-        if (p.game) {
-            if(p.game.timestamps.start){
-                //get time difference in hours and restrict it to two decimals
-                let timePlayed = (Math.abs(p.game.timestamps.start - new Date()) / 36e5).toFixed(2);
-                let time = timePlayed.split('.');
-                let hours = time[0];
-                let minutes = ((time[1]*60)/100).toFixed(0); 
-                let output = `${hours} hours and ${minutes} minutes`
-                if(hours == 0)
-                    output = `${minutes} minutes`;
-                //add playtime field
-                fields.push({ "name": "Time", "value": output });
+        /*
+            note: these awkward nested ifs are to error check for many possibilities here 
+            when bots are online they have a game but no timestamps
+            when users are online in an app but not a game (ie: spotify) they have timestamps but no start
+            when users are online in game they have a game, timestamp, and start 
+
+            these nested ifs make sure all fields exist before accessing the lowest layer to display playtime
+            there is probably a better way to do this
+        */
+        if(p.game){
+            if(p.game.timestamps){
+                if(p.game.timestamps.start){
+                    //get time difference in hours and restrict it to two decimals
+                    let timePlayed = (Math.abs(p.game.timestamps.start - new Date()) / 36e5).toFixed(2);
+                    let time = timePlayed.split('.');
+                    let hours = time[0];
+                    let minutes = ((time[1]*60)/100).toFixed(0); 
+                    let output = `${hours} hours and ${minutes} minutes`
+                    if(hours == 0)
+                        output = `${minutes} minutes`;
+                    //add playtime field
+                    fields.push({ "name": "Time", "value": output });
+                }
             }
-        }
+        }   
     }
 
     const embed = {
