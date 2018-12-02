756016a219681ae92e4c9a68d7ef2c38be4f2a47
DESKTOP\Frank <fmiskuly19@jcu.edu>
2018-12-02 15:46:35 -0500

    gamers rise up command for egan

---
+++ commands/riseup.js
@@ -0,0 +1,9 @@
+module.exports.run = (bot, message,args) => {
+    message.channel.send(`Gamers rise up!`)
+};
+
+module.exports.info = {
+    name: "riseup",
+    usage: ".riseup @user 'channelname'",
+    description: "#StopVeronica2k18"
+};
